311045514f0f49b39f7a10a69eefbb50198f5f23
afiqkiminaa <afiq654321@gmail.com>
2024-12-25 01:41:23 +0800

    Halfway backend signup page

---
+++ app/src/main/java/com/example/sisterlyapp/ReadWriteUserDetails.java
@@ -0,0 +1,11 @@
+package com.example.sisterlyapp;
+
+public class ReadWriteUserDetails {
+    public String fullname, username, mobile;
+
+    public ReadWriteUserDetails(String textFullname, String textUsername, String textMobile){
+        this.fullname = textFullname;
+        this.username = textUsername;
+        this.mobile = textMobile;
+    }
+}
--- app/src/main/java/com/example/sisterlyapp/SignupActivity.java
+++ app/src/main/java/com/example/sisterlyapp/SignupActivity.java
@@ -1,7 +1,10 @@
 package com.example.sisterlyapp;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +14,23 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -22,6 +38,7 @@ public class SignupActivity extends AppCompatActivity {
                      editTextRegisterMobile;
     private ProgressBar progressBar;
     private RadioButton radioButtonAgree;
+    private static final String TAG = "SignupActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +50,7 @@ public class SignupActivity extends AppCompatActivity {
 
         Toast.makeText(SignupActivity.this, "You can register now", Toast.LENGTH_LONG).show();
 
+        progressBar = findViewById(R.id.progressBar);
         editTextRegisterFullName = findViewById(R.id.ETFullName);
         editTextRegisterUsername = findViewById(R.id.ETUsername);
         editTextRegisterEmail = findViewById(R.id.ETEmail);
@@ -96,5 +114,64 @@ public class SignupActivity extends AppCompatActivity {
     }
     // User register using the credentials given
     private void registerUser (String textFullname, String textUsername, String textEmail, String textPwd, String textMobile){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(textEmail, textPwd).addOnCompleteListener(SignupActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(SignupActivity.this, "User registered successfully", Toast.LENGTH_LONG).show();
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+
+                            //Enter user data into the Firebase Realtime Database.
+                            ReadWriteUserDetails writeUserDetails = new ReadWriteUserDetails(textFullname, textUsername,textMobile);
+
+                            //Extracting user reference from Database for "Registered Users"
+                            DatabaseReference referenceProfile = FirebaseDatabase.getInstance().getReference("Registered Users");
+
+                            referenceProfile.child(firebaseUser.getUid()).setValue(writeUserDetails).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if (task.isSuccessful()) {
+                                        //Send Verification EMail
+                                        firebaseUser.sendEmailVerification();
+
+                                        Toast.makeText(SignupActivity.this, "User registered successfully. Please verify your email", Toast.LENGTH_LONG).show();
+
+//                                        //Open User profile after successful registration
+//                                        Intent intent = new Intent(SignupActivity.this, UserProfileActivity.class);
+//                                        //To prevent user from returning back to signup activity on pressing back button after registration
+//                                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//                                        startActivity(intent);
+//                                        finish();  // to close Signup Activity
+//                                    } else {
+//                                        Toast.makeText(SignupActivity.this, "User registered failed. Please try again", Toast.LENGTH_LONG).show();
+//                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+
+
+                        } else {
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthWeakPasswordException e) {
+                                editTextRegisterPwd.setError("Your password is to weak, Kingly use a mix of alphabets, number and special character");
+                                editTextRegisterPwd.requestFocus();
+                            } catch (FirebaseAuthInvalidCredentialsException e){
+                                editTextRegisterPwd.setError("Your email is invalid or already used. Kindly re-enter.");
+                                editTextRegisterPwd.requestFocus();
+                            } catch (FirebaseAuthUserCollisionException e) {
+                                editTextRegisterPwd.setError("User is already registered with this email. Use another email.");
+                                editTextRegisterPwd.requestFocus();
+                            } catch (Exception e) {
+                                Log.e(TAG, e.getMessage());
+                                Toast.makeText(SignupActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        }
+                    }
+                });
     }
 }
