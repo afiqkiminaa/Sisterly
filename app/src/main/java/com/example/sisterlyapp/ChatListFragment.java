ade88e58b874bd75ba25744d33a5a1f400a23c85
afiqkiminaa <afiq654321@gmail.com>
2025-01-12 15:24:47 +0800

    Module Afiq & Marsya Perfectly Completed

--- app/src/main/java/com/example/sisterlyapp/ChatListFragment.java
+++ app/src/main/java/com/example/sisterlyapp/ChatListFragment.java
@@ -1,5 +1,6 @@
 package com.example.sisterlyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +10,95 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ChatListFragment extends Fragment {
+import com.example.sisterlyapp.adapters.UsersAdapter;
+import com.example.sisterlyapp.databinding.ActivityChatListBinding;
+import com.example.sisterlyapp.listeners.UserListener;
+import com.example.sisterlyapp.models.User;
+import com.example.sisterlyapp.utilities.Constants;
+import com.example.sisterlyapp.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatListFragment extends Fragment implements UserListener {
+
+    private ActivityChatListBinding binding;
+    private PreferenceManager preferenceManager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the fragment layout
-        return inflater.inflate(R.layout.activity_chat_list, container, false);
+        binding = ActivityChatListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        preferenceManager = new PreferenceManager(requireContext());
+        getUsers();
+    }
+
+    private void getUsers() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    loading(false);
+                    String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        List<User> users = new ArrayList<>();
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                            if (currentUserId.equals(queryDocumentSnapshot.getId())) {
+                                continue;
+                            }
+                            User user = new User();
+                            user.name = queryDocumentSnapshot.getString(Constants.KEY_USERNAME);
+                            user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            user.id = queryDocumentSnapshot.getId();
+                            users.add(user);
+                        }
+                        if (users.size() > 0) {
+                            UsersAdapter usersAdapter = new UsersAdapter(users, this);
+                            binding.usersRecyclerView.setAdapter(usersAdapter);
+                            binding.usersRecyclerView.setVisibility(View.VISIBLE);
+                        } else {
+                            showErrorMessage();
+                        }
+                    } else {
+                        showErrorMessage();
+                    }
+                });
+    }
+
+    private void showErrorMessage() {
+        binding.textErrorMessage.setText(String.format("%s", "No user available"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    private void loading(Boolean isLoading) {
+        if (isLoading) {
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onUserClicked(User user) {
+        Intent intent = new Intent(requireContext(), ChatActivity.class);
+        intent.putExtra(Constants.KEY_USER, user);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
-
